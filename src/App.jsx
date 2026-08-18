d168bfa01213e7f221d3095a0b12152fcb10239f
Devesh804 <deveshc280@gmail.com>
2026-08-19 00:47:59 +0530

    Nested Routing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import About from './Pages/About';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact'
 import Navbar from './components/Navbar';
+import Men from './Pages/Men'
+import Women from './Pages/Women'
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
             <Route path='/contact' element={<Contact />} />
+            <Route path='/contact/Men' element={<Men />} />
+            <Route path='/contact/Women' element={<Women />} />
         </Routes>
     </div>
   )
